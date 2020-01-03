d134b3864b9bbb7d42f3a0ae054c9fc451545a13
Houston Perrett <hp@houstonp.com>
2020-01-02 18:47:57 -0600

    Added additional stochastic tests.

--- tests/testthat/test-stochastics.R
+++ tests/testthat/test-stochastics.R
@@ -8,13 +8,34 @@ test_that("generate_stochastics gives errors", {
 
 
 test_that("correlate_stochastics gives errors", {
-  stochastics <- matrix(rep(1,12),nrow=4)
-  corr <- matrix(c(1,0,0,0,1,0,0,0,1), nrow = 9)
-  
-  expect_error(correlate_stochastics(stochastics,corr))
-  
   stochastics <- matrix(rep(1,12),nrow=3)
   corr <- matrix(c(1,0,0,0,1,0,0,0,1), nrow = 3)
   
   expect_error(correlate_stochastics(stochastics,corr))
+  
+  stochastics <- matrix(rep(1,12),nrow=4)
+  corr <- matrix(c(1,0,0,0,1,0,0,0,1), nrow = 1)
+  
+  expect_error(correlate_stochastics(stochastics,corr))
+})
+
+test_that("generate_stochastics creates matrix", {
+  tst <- generate_stochastics(4,3)
+  
+  expect_is(tst, "matrix")
+  expect_equal(dim(tst)[1], 4)
+  expect_equal(dim(tst)[2], 3)
+  
+  tst <- generate_stochastics(4)
+  
+  expect_is(tst, "matrix")
+  expect_equal(dim(tst)[1], 4)
+  expect_equal(dim(tst)[2], 3)
+})
+
+test_that("correlate_stochastics creates correlated matrix", {
+  ones <- matrix(rep(1,9),nrow=3)
+  corr <- matrix(c(1,0.5,0,0.5,1,0,0,0,1),nrow=3)
+  
+  expect_equal(correlate_stochastics(ones,corr), matrix(c(rep(1,3),rep(1.366025,3),rep(1,3)),nrow=3), tolerance=1e-06)
 })
