8d76451debc415f71e7500f0c5210900808e46bb
Houston Perrett <hp@houstonp.com>
2020-01-05 12:16:58 -0600

    Changed generate_stochastics to reference Matrix library matrix class instead of base R.
    Changed processes to be rows instead of columns.
    
    Rearranged matrix multiplication due to change in rows and columns.

--- R/stochastics.R
+++ R/stochastics.R
@@ -14,7 +14,7 @@ generate_stochastics <- function(months, processes=3){
   if (months < 1) stop(paste0("The number of projection months must be greater than 0; Not ",months,"."), call. = FALSE) 
   if (processes < 1) stop(paste0("The number of stochastic processes must be greater than 0; Not ",processes,"."), call. = FALSE) 
   
-  matrix(rnorm(processes * months), nrow = months, ncol = processes)
+  Matrix(rnorm(processes * months), nrow = processes)
 }
 
 #' Correlate Stochastics
@@ -31,8 +31,8 @@ correlate_stochastics <- function(stochastics, correlation){
   stochastics <- as.matrix(stochastics)
   correlation <- as.matrix(correlation)
   
-  if (ncol(stochastics) != ncol(correlation)) stop("correlation must have the same number of columns as stochastic processes.")
+  if (nrow(stochastics) != nrow(correlation)) stop("correlation must have the same number of columns as stochastic processes.")
   if (nrow(correlation) != ncol(correlation)) stop("correlation must be a square matrix.")
   
-  stochastics %*% chol(correlation)
+  correlation %*% stochastics
 }
